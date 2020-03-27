3adcacc255b31fa7bfc98705d906cdc16e9fe7cb
kampretcode <manorder123@gmail.com>
2020-03-27 10:17:22 +0700

    test deploy

---
+++ test/test_program.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestRunning(unittest.TestCase):
+
+	def test_variable(self):
+		pass
+
+	def test_program_running(self):
+		pass
+
+
+if __name__ == '__main__':
+
+	unittest.main()
