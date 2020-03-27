d8c081f7003bfba94f7e3b53e43c3dc67a40dbeb
kampretcode <manorder123@gmail.com>
2020-03-27 09:34:24 +0700

    commit pertama

---
+++ deploy.py
@@ -0,0 +1,11 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({'parents': [{'id': '1Vxdx6T9_w4bEtvse8aLDL_FIMhqf6XCV'}], 'title': 'test.zip'})
+file1.SetContentFile('dist/test.zip')
+file1.Upload()
