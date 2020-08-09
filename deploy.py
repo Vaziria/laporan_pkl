0246c096df91597b101daf9aed9ba0c44f7dc6b2
kampretcode <manorder123@gmail.com>
2020-08-09 19:50:58 +0700

    change with version

--- deploy.py
+++ deploy.py
@@ -1,3 +1,6 @@
+import subprocess
+import os
+
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -6,13 +9,30 @@ from logger import Logger
 
 logger = Logger(__name__)
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
 
-drive = GoogleDrive(gauth)
+def get_version():
+	# git describe --tags
+	label = subprocess.check_output(["git", "describe", "--tags"]).strip()
+	return label
 
-file1 = drive.CreateFile({'parents': [{'id': _config.get('folderid')}], 'title': _config.get('appname')})
-file1.SetContentFile(_config.get('target_upload'))
-file1.Upload()
+def run():
+	version = str(get_version())
+	appname = _config.get('appname').format(version=version)
+	
+	gauth = GoogleAuth()
+	gauth.LocalWebserverAuth()
 
-logger.info('uploading file finished')
+	drive = GoogleDrive(gauth)
+
+	file1 = drive.CreateFile({'parents': [{'id': _config.get('folderid')}], 'title': appname})
+	file1.SetContentFile(_config.get('target_upload'))
+	file1.Upload()
+
+	logger.info('uploading file finished')
+
+if __name__ == '__main__':
+
+	version = get_version()
+	logger.info('uploading {}'.format(str(version)))
+
+	run()
